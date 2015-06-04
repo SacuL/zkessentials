5225a01e458ecbfeaa795fe8af75b9ad41a9df03
Lucas Saar Cerqueira <lucas44r@gmail.com>
2015-06-03 23:27:20 -0300

    Ajustes no Conf

--- src/main/java/br/ufjf/hydronode/Config.java
+++ src/main/java/br/ufjf/hydronode/Config.java
@@ -2,14 +2,15 @@ package br.ufjf.hydronode;
 
 public class Config {
 
-	public static final String urlSOS = "http://localhost:8182/52n-sos-webapp/service";
-	// public static final String urlServidor =
-	// "http://www.hydronode.ufjf.br/swe";
+	public static final String urlSOS = "http://localhost:8080/52n-sos-webapp/service";
 	public static final String urlServidor = "localhost:8080/essentials/swe";
 	public static final String observation = "/leitura/";
 	public static final String offering = "/sensor/";
 	public static final String procedure = "/estacao/";
 	public static final String featureOfInterest = "/caracteristica/";
 	public static final String observableProperty = "/propriedadeobservada/";
+
+	// inserir a key restringir para somente o ip do servidor
+	public static final String google_public_api_key = "";
 
 }
